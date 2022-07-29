4998c62e1c95295f786b50aa0bdc7a3b894f0c21
codak77 <seanchi77@gmail.com>
2022-07-29 07:00:54 +0100

    trying in cloud

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t nanodegree-project4 .
+
 
 # Step 2: 
 # List docker images
+docker images ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 nanodegree-project4 
